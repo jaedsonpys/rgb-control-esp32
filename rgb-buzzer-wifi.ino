441bf4e10c39b66f5709211aa3a52eaf1a338517
Jaedson Silva <contacanal798@gmail.com>
2022-05-14 22:13:58 -0300

    removing class from main code

--- rgb-buzzer-wifi.ino
+++ rgb-buzzer-wifi.ino
@@ -1,4 +1,5 @@
 #include <analogWrite.h>
+#include "rgb.h"
 
 const int redLedPin = 33;
 const int greenLedPin = 27;
@@ -6,44 +7,13 @@ const int blueLedPin = 14;
 
 const int buzzerPin = 32;
 
-class RGB {
-  private:
-    int redLedPin, greenLedPin, blueLedPin;
-  
-  public:
-    RGB(int redPin, int greenPin, int bluePin) {
-      pinMode(redPin, OUTPUT);
-      pinMode(greenPin, OUTPUT);
-      pinMode(bluePin, OUTPUT);
-
-      redLedPin = redPin;
-      greenLedPin = greenPin;
-      blueLedPin = bluePin;
-    }
-  
-    void red() {
-      digitalWrite(this->redLedPin, !digitalRead(redLedPin));
-    }
-
-    void green() {
-      digitalWrite(this->greenLedPin, !digitalRead(greenLedPin));
-    }
-
-    void blue() {
-      digitalWrite(this->blueLedPin, !digitalRead(blueLedPin));
-    }
-
-    void rgb(int red, int green, int blue) {
-      analogWrite(this->redLedPin, red);
-      analogWrite(this->greenLedPin, green);
-      analogWrite(this->blueLedPin, blue);
-    }
-};
-
 RGB rgb(redLedPin, greenLedPin, blueLedPin);
 
 void setup(){
   pinMode(buzzerPin, OUTPUT);
+  rgb.red();
+  rgb.green();
+  rgb.blue();
 }
 
 void loop() {
