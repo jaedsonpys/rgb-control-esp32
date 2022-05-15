4df64f0c2841ab31712ad2e32d5888f29cc92d37
Jaedson Silva <contacanal798@gmail.com>
2022-05-15 02:15:08 -0300

    adding gitignore

--- controller/main.py
+++ controller/main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter.colorchooser import Chooser
+from tkinter.colorchooser import askcolor
 
 from controller import Controller
 
@@ -10,8 +10,16 @@ root.geometry('300x300')
 
 ct = Controller('192.168.0.108', 80)
 
+
+def rgb_selector():
+    colors = askcolor(title='RGB Color')
+    rgb = colors[0]
+    ct.rgb(int(rgb[0]), int(rgb[1]), int(rgb[2]))
+
+
 ttk.Button(root, text='RED', command=ct.on_red).grid(column=0, row=3)
 ttk.Button(root, text='GREEN', command=ct.on_green).grid(column=0, row=4)
 ttk.Button(root, text='BLUE', command=ct.on_blue).grid(column=0, row=5)
+ttk.Button(root, text='RGB', command=rgb_selector).grid(column=0, row=6)
 
 root.mainloop()
