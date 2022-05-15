02709180aa1cdedb5c9cf662b53ed3b40235803a
Jaedson Silva <contacanal798@gmail.com>
2022-05-14 22:58:38 -0300

    connecting to wifi in esp32

--- rgb-buzzer-wifi.ino
+++ rgb-buzzer-wifi.ino
@@ -1,3 +1,7 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <ESPmDNS.h>
+
 #include "rgb.h"
 
 const int redLedPin = 33;
@@ -6,13 +10,30 @@ const int blueLedPin = 14;
 
 const int buzzerPin = 32;
 
+// wifi data
+const char *ssid = "test";
+const char *password = "test";
+
 RGB rgb(redLedPin, greenLedPin, blueLedPin);
 
 void setup(){
   pinMode(buzzerPin, OUTPUT);
-  rgb.red();
-  rgb.green();
-  rgb.blue();
+
+  // connecting wifi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  // led signal to WiFi connected
+  for(int i = 0; i < 6; i++) {
+    rgb.green();
+    delay(200);
+    rgb.green();
+    delay(200);
+  }
 }
 
 void loop() {
