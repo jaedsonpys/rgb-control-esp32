b57cde3f706b197f9aeb75a660aba6f5398763bf
Jaedson Silva <contacanal798@gmail.com>
2022-05-14 22:01:15 -0300

    adding constructor to set pins and using analogWrite library

--- rgb-buzzer-wifi.ino
+++ rgb-buzzer-wifi.ino
@@ -1,3 +1,5 @@
+#include <analogWrite.h>
+
 const int redLedPin = 33;
 const int greenLedPin = 27;
 const int blueLedPin = 14;
@@ -5,33 +7,42 @@ const int blueLedPin = 14;
 const int buzzerPin = 32;
 
 class RGB {
+  private:
+    int redLedPin, greenLedPin, blueLedPin;
+  
   public:
+    RGB(int redPin, int greenPin, int bluePin) {
+      pinMode(redPin, OUTPUT);
+      pinMode(greenPin, OUTPUT);
+      pinMode(bluePin, OUTPUT);
+
+      redLedPin = redPin;
+      greenLedPin = greenPin;
+      blueLedPin = bluePin;
+    }
+  
     void red() {
-      digitalWrite(redLedPin, !digitalRead(redLedPin));
+      digitalWrite(this->redLedPin, !digitalRead(redLedPin));
     }
 
     void green() {
-      digitalWrite(greenLedPin, !digitalRead(greenLedPin));
+      digitalWrite(this->greenLedPin, !digitalRead(greenLedPin));
     }
 
     void blue() {
-      digitalWrite(blueLedPin, !digitalRead(blueLedPin));
+      digitalWrite(this->blueLedPin, !digitalRead(blueLedPin));
     }
 
     void rgb(int red, int green, int blue) {
-      analogWrite(redLedPin, red);
-      analogWrite(greenLedPin, green);
-      analogWrite(blueLedPin, blue);
+      analogWrite(this->redLedPin, red);
+      analogWrite(this->greenLedPin, green);
+      analogWrite(this->blueLedPin, blue);
     }
 };
 
-RGB rgb;
+RGB rgb(redLedPin, greenLedPin, blueLedPin);
 
-void setup() {
-  pinMode(redLedPin, OUTPUT);
-  pinMode(blueLedPin, OUTPUT);
-  pinMode(greenLedPin, OUTPUT);
-
+void setup(){
   pinMode(buzzerPin, OUTPUT);
 }
 
