28104da2810429036de549cc055c4165ee960048
Jaedson Silva <contacanal798@gmail.com>
2022-05-14 21:40:50 -0300

    adding RGB led and buzzer setup

---
+++ rgb-buzzer-wifi.ino
@@ -0,0 +1,17 @@
+const int redLedPin = 33;
+const int greenLedPin = 27;
+const int blueLedPin = 14;
+
+const int buzzerPin = 32;
+
+void setup() {
+  pinMode(redLedPin, OUTPUT);
+  pinMode(blueLedPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
+
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+  
+}
